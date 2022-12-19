2435763fd25f0333015a910a47c9023c7aebbc8c
Apsie09 <61016270+Apsie09@users.noreply.github.com>
2022-12-19 18:02:09 +0200

    Add files via upload
    DA GO DUHATE NAPRAVIH Go SLED 3 SEDMICIIII

---
+++ VacationManager-main/VacationManager/DataLayer/ProjectDBContext.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class ProjectDBContext : DbContext
+    {
+        public ProjectDBContext() : base()
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-TTI2M5B;Database=myDataBase;Uid=myUsername;Pwd=myPassword;");
+            //optionsBuilder.UseSqlServer(@"Server=;Database=;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Server=;Database=;Trusted_Connection=True;");
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamLeader> TeamLeaders { get; set; }
+        public DbSet<CEO> CEOs { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<VacationDoc> VacationDocs { get; set; }
+    }
+}
---
+++ VacationManager-main/VacationManager/ServiceLayer/DBContextManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer;
+
+namespace ServiceLayer
+{
+    public class DBContextManager
+    {
+        private static CEOContext _context;
+        private static DeveloperContext _developerContext;
+        private static TeamContext _teamContext;
+        private static TeamLeaderContext _teamleaderContext;
+        private static VacationDocContext _vacationdocContextContext;
+
+        public static CEOContext CreateContext()
+        {
+            _context = new CEOContext();
+            return _context;
+        }
+
+        public static CEOContext GetContext()
+        {
+            return _context;
+        }
+      
+
+        //DeveloperContexts
+        public static DeveloperContext CreateDeveloperContext(CEOContext context)
+        {
+            _developerContext = new DeveloperContext(context);
+            return _developerContext;
+        }
+
+        public static DeveloperContext GetDeveloperContext()
+        {
+            return _developerContext;
+        }
+
+        //TeamContext
+        public static TeamContext CreateCommentContext(CEOContext context)
+        {
+            _teamContext = new TeamContext(context);
+            return _teamContext;
+        }
+
+        public static TeamContext GetCommentContext()
+        {
+            return _teamContext;
+        }
+
+        //TeamLeaderContext
+        public static TeamLeaderContext CreateTeamLeaderContext(CEOContext context)
+        {
+            _teamleaderContext = new TeamLeaderContext(context);
+            return _teamleaderContext;
+        }
+        public static TeamLeaderContext GetTeamLeaderContext()
+        {
+            return _teamleaderContext;
+        }
+
+        //VacationDocContext
+        public static VacationDocContext CreateVacationDocContext(CEOContext context)
+        {
+            _vacationdocContextContext = new VacationDocContext(context);
+            return _vacationdocContextContext;
+        }
+        public static VacationDocContext GetVacationDocContext()
+        {
+            return _vacationdocContextContext;
+        }
+
+
+    }
+}
